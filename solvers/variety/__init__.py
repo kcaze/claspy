3f7ffb44e0ac431499e2f6b9131df8ac491a4e13
Herman Chau <beijingherman@gmail.com>
2019-09-02 15:36:35 +0000

    Added akari solver

--- solvers/variety/__init__.py
+++ solvers/variety/__init__.py
@@ -1,10 +1,12 @@
 from _base_ import parseURL
 
+import akari
 import slither
 import starbattle
 import sudoku
 
 solvers = {
+  'akari': akari.Akari,
   'slither': slither.Slither,
   'starbattle': starbattle.Starbattle,
   'sudoku': sudoku.Sudoku,
